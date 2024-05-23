3a435d1ff609c53a2762f6455d14ea4921d54e9b
xmdi <xmdi@protonmail.com>
2024-05-22 22:24:15 -0400

    naca4 work

--- src/naca4.m
+++ src/naca4.m
@@ -1,15 +1,15 @@
 function [P]=naca4(naca4_airfoil)
-	
+
 	m=str2num(naca4_airfoil.ID(1))/100;
 	p=str2num(naca4_airfoil.ID(2))/10;
 	t=str2num(naca4_airfoil.ID(3:4))/100;
-	
+
 	x=(1-cos(linspace(0,pi,naca4_airfoil.panels+1)))/2;
 
 	yt=5*t*(.2969*sqrt(x)-.1260*x-.3516*x.^2+.2843*x.^3-.1015*x.^4);
 
-	xAft=x(find(x<=p));
-	xFwd=x(find(x>p));
+	xFwd=x(find(x<=p));
+	xAft=x(find(x>p));
 
 	if p==0
 		xUAF=x;
@@ -22,19 +22,19 @@ function [P]=naca4(naca4_airfoil)
 		yc=[ycFwd ycAft];
 
 		dycFwd=(2*m/p^2)*(p-xFwd);
-		dycAft=(2*m/(1-p)^2)*(p-xAft);	
-		dyc=[dycFwd dycAft];	
+		dycAft=(2*m/(1-p)^2)*(p-xAft);
+		dyc=[dycFwd dycAft];
 		o=atan(dyc);
-	
+
 		xUAF=x-yt.*sin(o);
 		yUAF=yc+yt.*cos(o);
 		xLAF=x+yt.*sin(o);
 		yLAF=yc-yt.*cos(o);
 	end
-	
+
 	xFinal=[fliplr(xUAF) xLAF(2:end)];
 	yFinal=[fliplr(yUAF) yLAF(2:end)];
-	
-	P=[xFinal;yFinal];
+
+	P=[xFinal;yFinal]';
 
 end
--- test.m
+++ test.m
@@ -11,6 +11,13 @@ Ixx
 Iyy
 Ixy
 
-fill (body.vertices(:,1), body.vertices(:,2), "r");
-axis ([-0.5 1.5, -0.5 1.5])
-axis equal
+naca_airfoil=struct;
+naca_airfoil.ID='2412';
+naca_airfoil.panels=100;
+P=naca4(naca_airfoil);
+plot(P(:,1),P(:,2));axis equal
+
+
+%fill (body.vertices(:,1), body.vertices(:,2), "r");
+%axis ([-0.5 1.5, -0.5 1.5])
+%axis equal
