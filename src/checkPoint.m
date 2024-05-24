2f1b0aa479758de48d6e7b2c5ff0acc34a319bcc
Matt DiPalma <31139829+xmdi@users.noreply.github.com>
2024-05-24 01:07:43 -0400

    checkPoint start, test device

---
+++ src/checkPoint.m
@@ -0,0 +1,15 @@
+function []=checkPoint(point)
+
+if ~isnumeric(point.x)
+error("Invalid x position.");
+elseif ~isnumeric(point.y)
+error("Invalid y position.");
+elseif ~islogical(x.visibility)
+error("Invalid visibility.");
+end
+
+
+
+
+
+end
