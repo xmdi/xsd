8eb054a695331bf8db7be981a842999e7abfcf90
xmdi <xmdi@protonmail.com>
2024-08-23 23:58:01 -0400

    plot

--- test_mesh.m
+++ test_mesh.m
@@ -12,8 +12,9 @@ section=initSection(0,0,0,0);
 [p4,section]=createPointInSpace(section,13,-5);
 [path1,section]=createPathFromPoints(section,[p1,p2,p3,p4,p1]);
 [face1,section]=createFaceFromPaths(section,[path1]);
+[section]=setVisibilities(section,[p1,p2,p3,p4,path1],false);
 
-%drawSection(section);
+drawSection(section);
 
 [nodeArray,elementArray]=meshFace(section,section.faces(1),.4);
 
